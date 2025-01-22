a4b2cd1b148d3192d7119b1d6f040b2b757af262
Xiaoming Wang <dawnseeker8@gmail.com>
2025-01-22 19:58:10 +0800

    feat: revent `ledger-bridge.js` to `main` version.

--- ledger-bridge.js
+++ ledger-bridge.js
@@ -206,7 +206,7 @@ export default class LedgerBridge {
       this.sendMessageToExtension({
         action: replyAction,
         success: false,
-        payload: { error: JSON.stringify(e) },
+        payload: { error: e },
         messageId,
       });
     } finally {
@@ -235,7 +235,7 @@ export default class LedgerBridge {
       this.sendMessageToExtension({
         action: replyAction,
         success: false,
-        payload: { error: JSON.stringify(e) },
+        payload: { error: e },
         messageId,
       });
     } finally {
@@ -261,7 +261,7 @@ export default class LedgerBridge {
       this.sendMessageToExtension({
         action: replyAction,
         success: false,
-        payload: { error: JSON.stringify(e) },
+        payload: { error: e },
         messageId,
       });
     } finally {
@@ -287,7 +287,7 @@ export default class LedgerBridge {
       this.sendMessageToExtension({
         action: replyAction,
         success: false,
-        payload: { error: JSON.stringify(e) },
+        payload: { error: e },
         messageId,
       });
     } finally {
