f4788d4a219b209e158b45ead8061fdc109889d1
Xiaoming Wang <dawnseeker8@gmail.com>
2025-03-03 13:24:52 +0000

    fix: add rollback to support nano s.

--- ledger-bridge.js
+++ ledger-bridge.js
@@ -2,6 +2,7 @@ import TransportWebUSB from '@ledgerhq/hw-transport-webusb';
 import TransportWebHID from '@ledgerhq/hw-transport-webhid';
 import LedgerEth from '@ledgerhq/hw-app-eth';
 import WebSocketTransport from '@ledgerhq/hw-transport-http/lib/WebSocketTransport';
+import { SignTypedDataVersion, TypedDataUtils } from '@metamask/eth-sig-util';
 
 // URL which triggers Ledger Live app to open and handle communication
 const BRIDGE_URL = 'ws://localhost:8435';
@@ -274,7 +275,31 @@ export default class LedgerBridge {
   async signTypedData(replyAction, hdPath, message, messageId) {
     try {
       await this.makeApp();
-      const res = await this.app.signEIP712Message(hdPath, message);
+      let res;
+      try {
+        res = await this.app.signEIP712Message(hdPath, message);
+      } catch (signError) {
+        // Fallback to signEIP712HashedMessage if signEIP712Message fails (e.g., for Nano S)
+        // Extract hashStructMessageHex and domainSeparatorHex from the message object
+        const domainSeparatorHex = TypedDataUtils.hashStruct(
+          'EIP712Domain',
+          message.domain,
+          message.types,
+          SignTypedDataVersion.V4,
+        ).toString('hex');
+        const hashStructMessageHex = TypedDataUtils.hashStruct(
+          message.primaryType,
+          message.message,
+          message.types,
+          SignTypedDataVersion.V4,
+        ).toString('hex');
+
+        res = await this.app.signEIP712HashedMessage(
+          hdPath,
+          domainSeparatorHex,
+          hashStructMessageHex,
+        );
+      }
 
       this.sendMessageToExtension({
         action: replyAction,
